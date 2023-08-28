7306d2a1a0d3ed260158bc572f6dac6603ed79e5
Java-kokyu <qwe38280@gmail.com>
2023-08-29 00:06:59 +0900

    ReactJS로 영화 웹 서비스 만들기3 - TodoList & Coin converter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
+
+/* FIXME: 1. 간단한 TODO 제작
+function App() {
+    const [todo, setTodo] = useState("");
+    const [todoList, setTodoList] = useState([]);
+    const onChange = (event) => {
+        setTodo(event.target.value);
+    }
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (todo === "") return;
+        setTodoList(currentArray => [todo, ...currentArray]);
+        setTodo("");
+    }
+
+    console.log(todoList);
+
+    return (
+        <div>
+            <h1>My Todo ({todoList.length})</h1>
+            <form onSubmit={onSubmit}>
+                <input value={todo} onChange={onChange} type="text" placeholder="Write your todo"/>
+                <button>Add To Do</button>
+            </form>
+            <hr/>
+            <ul>
+                {createTodoList(todoList)}
+            </ul>
+
+        </div>
+    );
+}
+
+function createTodoList(list) {
+    return list.map((item, index) => <li key={index}>{item}</li>)
+}
+*/
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [loading, setLoading] = useState(true);
+    const [coins, setCoins] = useState([]);
+    const [dollar, setDollar] = useState(0);
+    const [index, setIndex] = useState("0");
+    const [convertedCoin, setConvertedCoin] = useState(0);
+    useEffect(() => {
+        fetch("https://api.coinpaprika.com/v1/tickers")
+            .then((response) => response.json())
+            .then((json) => {
+                setCoins(json);
+                setLoading(false);
+            });
+    }, []);
+
+    const onChange = (event) => {
+        setDollar(event.target.value);
+        setConvertedCoin(event.target.value / Math.fround(coins[index].quotes.USD.ath_price).toFixed(2))
+    }
+
+    const onSelect = (event) => {
+        console.log(event)
+        setIndex(event.target.value);
+    }
+    return (
+        <div>
+            <h1>The Coins! {loading ? null : coins.length}</h1>
+            {loading ? <strong>Loading...</strong> : null}
+            <hr />
+            <select value={index} onChange={onSelect} hidden={loading}>
+                {coins.map((coin, value) => <option value={value}>{coin.name} ({coin.symbol}):
+                    ${Math.fround(coin.quotes.USD.ath_price).toFixed(2)}</option>)}
+            </select>
+            <div hidden={loading}>
+                <label htmlFor="Your USD">$
+                    <input id="dollar" type="number" value={dollar} onChange={onChange} placeholder="Write Your $USD"/>
+                </label>
+                <h3>Converted Coin: {convertedCoin}</h3>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
